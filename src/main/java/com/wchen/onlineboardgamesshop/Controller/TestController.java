7552e92dc50c61690cb8ac7b51de43b72c2c08b0
paboooh <paboooh@gmail.com>
2023-06-25 00:38:56 +0200

    new

---
+++ src/main/java/com/wchen/onlineboardgamesshop/Controller/TestController.java
@@ -0,0 +1,14 @@
+package com.wchen.onlineboardgamesshop.Controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String test() {
+        return "Hello jenkins.";
+    }
+}
---
+++ src/test/java/com/wchen/onlineboardgamesshop/OnlineBoardGamesShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wchen.onlineboardgamesshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBoardGamesShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
