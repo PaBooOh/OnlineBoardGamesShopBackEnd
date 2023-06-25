2f6d6dd96842e685537d0795236e6aa8e1ef436d
paboooh <paboooh@gmail.com>
2023-06-26 01:14:42 +0200

    new

--- src/main/java/com/wchen/onlineboardgamesshop/Controller/TestController.java
+++ src/main/java/com/wchen/onlineboardgamesshop/Controller/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @GetMapping("/hello")
     public String test() {
-        return "Hello jenkins! Pipeline created!!! Go Go Go!";
+        return "Hello jenkins! Pipeline created!!! Go Go Go!!!!!!!";
     }
 }
